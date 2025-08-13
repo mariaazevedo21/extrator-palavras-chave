14bddb9d8e282d86eef45da00d86e1d509ae623b
mariaazevedo21 <m.azevedo21@escola.pr.gov.br>
2025-08-13 11:10:59 -0300

    script.js

--- script.js
+++ script.js
@@ -3,5 +3,34 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+const texto = document.querySelector('#entrada-de-texto').value;
+const campoResultado =doment.querySeletor ('#resultado-palvrachave');
+const palavrasChave = prossaTexto(texto);
+
+
+campoResultado.textoContent = palavrasChave. join (", ");
 }
+
+function processaTexto (texto){
+ let palvras = texto. split(/\P{L}+/u);
+ contraFrequencias(palavras) {
+ Let frequencias = {};
+ For (Let i of palavras);
+   frequencias [i] = 0;
+    for (let j of palavras) {
+        if (i == j) {
+            frequencias [i] ++;
+        }
+    }
+ }
+    
+console.log(frequencias);
+    return palavras;
+}
+
+function contraFrequencia(Palavras){
+    
+}
+
+    
+    
